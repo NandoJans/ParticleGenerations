c01f719802012c00669593b5fde0a92e2114e79d
NandoJans <90688887+NandoJans@users.noreply.github.com>
2023-03-21 11:51:53 +0100

    Update V0.8.3

--- src/app/services/interactables/generators/purple/particles.ts
+++ src/app/services/interactables/generators/purple/particles.ts
@@ -3,19 +3,19 @@ import {Num} from "../../../../num";
 export const purpleParticleGenerators = [
   {
     name: 'purple-generator-1', displayName: 'Purple Generator 1', auto: false, style: 'purple-style',
-    baseCost: new Num(1, 0), cost: new Num(1, 0), increase: new Num(1, 1), scaling: new Num(1, 1), bought: new Num(0, 0), currency: 'purpleParticles',
+    baseCost: new Num(1, 0), cost: new Num(1, 0), increase: new Num(5, 0), scaling: new Num(2, 0), bought: new Num(0, 0), currency: 'purpleParticles',
     generates: 'purpleVoid', baseMulMod: new Num(1, 0), baseMultiplier: new Num(5, 0), multiplier: new Num(5, 0), amount: new Num(0, 0) , type: 'purple-particles', resetId: 'purpleParticleGenerators', unlocked: false,
     requirement: ['holding', 'purples', new Num(1, 0)], nav: 'purple', subNav: 'purpleGenerators', globalMultiplier: 'purpleParticleGenerators',
   },
   {
     name: 'purple-generator-2', displayName: 'Purple Generator 2', auto: false, style: 'purple-style',
-    baseCost: new Num(1, 10), cost: new Num(1, 10), increase: new Num(1, 5), scaling: new Num(1, 1), bought: new Num(0, 0), currency: 'purpleParticles',
+    baseCost: new Num(1, 1), cost: new Num(1, 1), increase: new Num(2.5, 1), scaling: new Num(2, 0), bought: new Num(0, 0), currency: 'purpleParticles',
     generates: 'purple-generator-1', baseMulMod: new Num(1, 0), baseMultiplier: new Num(5, 0), multiplier: new Num(5, 0), amount: new Num(0, 0) , type: 'purple-particles', resetId: 'purpleParticleGenerators', unlocked: false,
     requirement: ['holding', 'purples', new Num(1, 0)], nav: 'purple', subNav: 'purpleGenerators', globalMultiplier: 'purpleParticleGenerators',
   },
   {
     name: 'purple-generator-3', displayName: 'Purple Generator 3', auto: false, style: 'purple-style',
-    baseCost: new Num(1, 25), cost: new Num(1, 25), increase: new Num(5, 12), scaling: new Num(1, 1), bought: new Num(0, 0), currency: 'purpleParticles',
+    baseCost: new Num(1, 2), cost: new Num(1, 2), increase: new Num(1.25, 2), scaling: new Num(2, 0), bought: new Num(0, 0), currency: 'purpleParticles',
     generates: 'purple-generator-2', baseMulMod: new Num(1, 0), baseMultiplier: new Num(5, 0), multiplier: new Num(5, 0), amount: new Num(0, 0) , type: 'purple-particles', resetId: 'purpleParticleGenerators', unlocked: false,
     requirement: ['holding', 'purples', new Num(1, 0)], nav: 'purple', subNav: 'purpleGenerators', globalMultiplier: 'purpleParticleGenerators',
   },
--- src/app/services/tick.service.ts
+++ src/app/services/tick.service.ts
@@ -104,7 +104,7 @@ export class TickService {
     //HoldingsService.set('yellowParticles', new Num(1, 5))
     //HoldingsService.set('greenParticles', new Num(1, 10))
     //HoldingsService.set('blueParticles', new Num(1.1, 110))
-    //HoldingsService.set('purpleParticles', new Num(1, 2))
+    //HoldingsService.set('purpleParticles', new Num(1, 3))
     //HoldingsService.set('yellows', new Num(5, 3))
     //HoldingsService.set('greens', new Num(1, 3))
     //HoldingsService.set('blues', new Num(3, 2))
