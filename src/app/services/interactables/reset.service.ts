a470b853dab24c831726d5e55d16d09ece433e48
NandoJans <90688887+NandoJans@users.noreply.github.com>
2023-05-03 10:53:48 +0200

    Fix

--- src/app/services/interactables/reset.service.ts
+++ src/app/services/interactables/reset.service.ts
@@ -99,6 +99,7 @@ export class ResetService {
     this.resetUpgrades('red-accelerators')
     this.resetUpgrades('red-particles')
     this.resetUpgrades('red-upgrades')
+    PrestigeLayersService.setValue('yellow', 'fastestGainPS', new Num(0, 0))
     DataManagerService.save()
     if (!HoldingsService.get('yellows').greq(new Num(5, 1))) App.next();
     if (resets === 'yellow') return;
@@ -116,6 +117,7 @@ export class ResetService {
     this.resetGenerators('nuclearDecay', 'amount')
     this.resetUpgrades('yellow-fusion')
     this.resetChallenges('yellow-challenges')
+    PrestigeLayersService.setValue('green', 'fastestGainPS', new Num(0, 0))
     DataManagerService.save()
     if (!HoldingsService.get('greens').greq(new Num(5, 1))) App.next();
     if (resets === 'green') return;
@@ -157,6 +159,7 @@ export class ResetService {
     HoldingsService.set('blueHydrogen', new Num(1, 0))
     this.resetGenerators('blueParticleGenerators', 'amount')
     this.resetGenerators('blue-neutrons', 'amount')
+    PrestigeLayersService.setValue('blue', 'fastestGainPS', new Num(0, 0))
     DataManagerService.save()
     if (!HoldingsService.get('blues').greq(new Num(5, 1)) && resets === 'blue') App.next();
     if (resets === 'blue') return;
