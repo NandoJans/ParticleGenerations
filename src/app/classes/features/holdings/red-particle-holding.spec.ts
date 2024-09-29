217f4921625f1a88dac1ec084983166217e87427
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 21:54:19 +0200

    applyNewKnowledge Create red particle holding

---
+++ src/app/classes/features/holdings/red-particle-holding.spec.ts
@@ -0,0 +1,7 @@
+import { RedParticleHolding } from './red-particle-holding';
+
+describe('RedParticleHolding', () => {
+  it('should create an instance', () => {
+    expect(new RedParticleHolding()).toBeTruthy();
+  });
+});
