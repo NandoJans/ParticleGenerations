9df899c8c24e5744937238b1f81f7932fa142199
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:59:52 +0200

    applyNewKnowledge Implemented red particle holding

--- src/app/classes/features/holdings/red-particle-holding.ts
+++ src/app/classes/features/holdings/red-particle-holding.ts
@@ -1,10 +1,21 @@
 import {Holding} from "../holding";
 import {Num} from "../../../num";
+import {Styles} from "../../enums/styles";
+import {HoldingDisplay} from "../../displays/holding-display";
+import {HoldingDisplayFactory} from "../../factories/holding-display-factory";
 
 export class RedParticleHolding extends Holding {
   name = 'redParticles';
   abbreviation = 'RP';
   amount = new Num(1, 1);
   startAmount: Num = new Num(1, 1);
-  effect = new Num(1, 0);
+
+  holdingDisplay: HoldingDisplay = HoldingDisplayFactory.start(this)
+    .withAmountPrefix('You have')
+    .withAmountSuffix('Red Particles')
+    .build();
+
+  getStyle(): Styles {
+    return Styles.RED;
+  }
 }
