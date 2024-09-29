e0ea30b73f36e4b1fe06b9f44320c82da1b38cc7
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:12:04 +0200

    applyNewKnowledge Implement Local Storage Helper

--- src/app/classes/helpers/local-storage-helper.ts
+++ src/app/classes/helpers/local-storage-helper.ts
@@ -1,2 +1,27 @@
 export class LocalStorageHelper {
+  category: string;
+  key: string;
+
+  constructor(category: string, key: string) {
+    this.category = category;
+    this.key = key;
+  }
+
+  exists(key: string): boolean {
+    return localStorage.getItem(`${this.category}-${this.key}-${key}`) !== null;
+  }
+
+  save(key: string, value: any): void {
+    localStorage.setItem(`${this.category}-${this.key}-${key}`, JSON.stringify(value))
+  }
+
+  saveMultiple(values: {[key: string]: any}): void {
+    for (let key in values) {
+      this.save(key, values[key]);
+    }
+  }
+
+  load(key: string, ifNotSet: any): any {
+    return JSON.parse(localStorage.getItem(`${this.category}-${this.key}-${key}`) || 'null') || ifNotSet;
+  }
 }
