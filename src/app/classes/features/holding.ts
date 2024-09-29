e522db2f5edca10fbafbeae97672af06080bab5b
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:08:27 +0200

    applyNewKnowledge Implement Holding logic

--- src/app/classes/features/holding.ts
+++ src/app/classes/features/holding.ts
@@ -1,2 +1,37 @@
-export class Holding {
+import {Num} from "../../num";
+import {LocalStorageHelper} from "../helpers/local-storage-helper";
+
+export abstract class Holding {
+  abstract name: string;
+  abstract amount: Num;
+  abstract startAmount: Num;
+  abstract effect: Num;
+  action: Function | undefined = undefined
+
+  private localStorageHelper: LocalStorageHelper
+
+  getSaveKey(): string {
+    return this.name;
+  }
+
+  constructor() {
+    this.localStorageHelper = new LocalStorageHelper('holding', this.getSaveKey())
+    this.tryLoad()
+  }
+
+  tryLoad(): void {
+    this.amount = this.localStorageHelper.load('amount', this.amount)
+    this.startAmount = this.localStorageHelper.load('startAmount', this.startAmount)
+    this.effect = this.localStorageHelper.load('effect', this.effect)
+  }
+
+  save(): void {
+    this.localStorageHelper.save('amount', this.amount)
+    this.localStorageHelper.save('startAmount', this.startAmount)
+    this.localStorageHelper.save('effect', this.effect)
+  }
+
+  printEffect(): string {
+    return this.effect.toString()
+  }
 }
