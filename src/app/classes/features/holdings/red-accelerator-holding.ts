87819939680460c49ec87063a0713656e510c300
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:17:06 +0200

    applyNewKnowledge Define red accelerator holding

--- src/app/classes/features/holdings/red-accelerator-holding.ts
+++ src/app/classes/features/holdings/red-accelerator-holding.ts
@@ -1,2 +1,13 @@
-export class RedAcceleratorHolding {
+import {Holding} from "../holding";
+import {Num} from "../../../num";
+
+export class RedAcceleratorHolding extends Holding {
+  name = 'redAccelerators';
+  abbreviation = 'RA';
+  amount = new Num(1, 1);
+  effect = new Num(1, 0);
+  startAmount: Num = new Num(1, 0);
+  override action(): any {
+
+  }
 }
