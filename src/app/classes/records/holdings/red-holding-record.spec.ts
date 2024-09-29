689ad9549aefcb72ea1bcfa5b3ec40794ec71de5
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 21:39:25 +0200

    applyNewKnowledge Create red holding record class

---
+++ src/app/classes/records/holdings/red-holding-record.spec.ts
@@ -0,0 +1,7 @@
+import { RedHoldingRecord } from './red-holding-record';
+
+describe('RedHoldingRecord', () => {
+  it('should create an instance', () => {
+    expect(new RedHoldingRecord()).toBeTruthy();
+  });
+});
