8f88a4164e1f071228569f569ae7592a3ca85991
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:53:44 +0200

    applyNewKnowledge Factory implemented for creating holding display

--- src/app/classes/factories/holding-display-factory.ts
+++ src/app/classes/factories/holding-display-factory.ts
@@ -1,2 +1,42 @@
+import {Holding} from "../features/holding";
+import {HoldingDisplay} from "../displays/holding-display";
+
 export class HoldingDisplayFactory {
+  amountPrefix: string = '';
+  amountSuffix: string = '';
+  effectPrefix: string = '';
+  effectSuffix: string = '';
+  holding: Holding;
+
+  constructor(holding: Holding) {
+    this.holding = holding;
+  }
+
+  static start(holding: Holding) {
+    return new HoldingDisplayFactory(holding);
+  }
+
+  withAmountPrefix(amountPrefix: string): HoldingDisplayFactory {
+    this.amountPrefix = amountPrefix;
+    return this;
+  }
+
+  withAmountSuffix(amountSuffix: string): HoldingDisplayFactory {
+    this.amountSuffix = amountSuffix;
+    return this;
+  }
+
+  withEffectPrefix(effectPrefix: string): HoldingDisplayFactory {
+    this.effectPrefix = effectPrefix;
+    return this;
+  }
+
+  withEffectSuffix(effectSuffix: string): HoldingDisplayFactory {
+    this.effectSuffix = effectSuffix;
+    return this;
+  }
+
+  build(): HoldingDisplay {
+    return new HoldingDisplay(this.amountPrefix, this.amountSuffix, this.effectPrefix, this.effectSuffix);
+  }
 }
