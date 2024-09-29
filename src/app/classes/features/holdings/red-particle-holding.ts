fd7a242edebe826f29cb789ed7b5da6705aa475b
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:17:13 +0200

    applyNewKnowledge Define red particle holding

--- src/app/classes/features/holdings/red-particle-holding.ts
+++ src/app/classes/features/holdings/red-particle-holding.ts
@@ -1,2 +1,10 @@
-export class RedParticleHolding {
+import {Holding} from "../holding";
+import {Num} from "../../../num";
+
+export class RedParticleHolding extends Holding {
+  name = 'redParticles';
+  abbreviation = 'RP';
+  amount = new Num(1, 1);
+  startAmount: Num = new Num(1, 1);
+  effect = new Num(1, 0);
 }
