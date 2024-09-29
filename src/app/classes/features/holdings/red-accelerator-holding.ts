a3e9c159c6409fdf67d768b488a6c799de77c27d
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:59:39 +0200

    applyNewKnowledge Implemented red accelerator holding

--- src/app/classes/features/holdings/red-accelerator-holding.ts
+++ src/app/classes/features/holdings/red-accelerator-holding.ts
@@ -1,13 +1,31 @@
 import {Holding} from "../holding";
 import {Num} from "../../../num";
+import {Styles} from "../../enums/styles";
+import {HoldingDisplayFactory} from "../../factories/holding-display-factory";
+import {HoldingDisplay} from "../../displays/holding-display";
+import {UpgradeService} from "../../../services/interactables/upgrade.service";
+import {GlobalMultipliersService} from "../../../services/globals/global-multipliers.service";
 
 export class RedAcceleratorHolding extends Holding {
   name = 'redAccelerators';
   abbreviation = 'RA';
   amount = new Num(1, 1);
-  effect = new Num(1, 0);
+  override effect = new Num(1, 0);
   startAmount: Num = new Num(1, 0);
-  override action(): any {
+  holdingDisplay: HoldingDisplay = HoldingDisplayFactory.start(this)
+    .withAmountPrefix('You have')
+    .withAmountSuffix('Red Accelerators')
+    .withEffectPrefix('They multiply Red Particle Generators by')
+    .build();
 
+  override action(): Num {
+    // @ts-ignore
+    let buffer: Num = (UpgradeService.getValue('red-accelerator-buffer', 'bought').greq(new Num(1, 0))) ? amount.pow(new Num(1.5, 0), false) : amount.div(new Num(1, 3), false).add(new Num(1, 0), false);
+    GlobalMultipliersService.correct('redParticleGenerators', buffer)
+    return buffer.copy();
+  }
+
+  getStyle(): Styles {
+    return Styles.RED;
   }
 }
