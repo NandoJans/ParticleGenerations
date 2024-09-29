8297f6031570f4f5d621dced8cd82f7aacfebfac
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:53:53 +0200

    applyNewKnowledge Changed object to class

--- src/app/classes/records/holdings/holding-record.ts
+++ src/app/classes/records/holdings/holding-record.ts
@@ -1,8 +1,7 @@
-import {Holding} from "../../features/holding";
 import {RedParticleHolding} from "../../features/holdings/red-particle-holding";
 import {RedAcceleratorHolding} from "../../features/holdings/red-accelerator-holding";
 
-export const HoldingRecord: {[key: string]: Holding} = {
-  redParticles: new RedParticleHolding(),
-  redAccelerators: new RedAcceleratorHolding(),
+export class HoldingRecord {
+  static redParticles     = new RedParticleHolding()
+  static redAccelerators  = new RedAcceleratorHolding()
 }
