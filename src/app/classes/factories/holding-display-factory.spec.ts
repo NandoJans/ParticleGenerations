3fac9b49713d19ad28bee374aa80c820f7bc66df
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-29 22:31:45 +0200

    applyNewKnowledge Create holding display factory for displaying html format of the holding display.

---
+++ src/app/classes/factories/holding-display-factory.spec.ts
@@ -0,0 +1,7 @@
+import { HoldingDisplayFactory } from './holding-display-factory';
+
+describe('HoldingDisplayFactory', () => {
+  it('should create an instance', () => {
+    expect(new HoldingDisplayFactory()).toBeTruthy();
+  });
+});
