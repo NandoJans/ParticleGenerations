c9fc83ca3ee82cdd0b8bf50854f965f516ad849e
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 17:55:36 +0200

    applyNewKnowledge Implement base yellow particles class

--- src/app/classes/features/holdings/yellow-particle-holding.ts
+++ src/app/classes/features/holdings/yellow-particle-holding.ts
@@ -1,2 +1,21 @@
-export class YellowParticleHolding {
+import {Holding} from "../holding";
+import {Num} from "../../../num";
+import {HoldingDisplay} from "../../displays/holding-display";
+import {HoldingDisplayFactory} from "../../factories/holding-display-factory";
+import {Styles} from "../../enums/styles";
+
+export class YellowParticleHolding extends Holding {
+  name: string = 'yellowParticles';
+  abbreviation: string = 'YP';
+  amount: Num = new Num(0, 0);
+  startAmount: Num = new Num(0, 0);
+  holdingDisplay: HoldingDisplay = HoldingDisplayFactory.start(this)
+    .withAmountPrefix('You have')
+    .withAmountSuffix('Yellow Particles')
+    .build();
+
+  getStyle(): Styles {
+    return Styles.YELLOW;
+  }
+
 }
