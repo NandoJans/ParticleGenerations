5a0ba969091689a9d74f5b59dcea28930f68fdef
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 18:12:51 +0200

    applyNewKnowledge implement Yellow Power Holding

--- src/app/classes/features/holdings/yellow-power-holding.ts
+++ src/app/classes/features/holdings/yellow-power-holding.ts
@@ -1,2 +1,37 @@
-export class YellowPowerHolding {
+import {Holding} from "../holding";
+import {Num} from "../../../num";
+import {HoldingDisplay} from "../../displays/holding-display";
+import {HoldingDisplayFactory} from "../../factories/holding-display-factory";
+import {Styles} from "../../enums/styles";
+import {GlobalMultipliersService} from "../../../services/globals/global-multipliers.service";
+
+export class YellowPowerHolding extends Holding {
+  name: string = 'yellowPower';
+  abbreviation: string = 'YW';
+  amount: Num = new Num(1, 0);
+  startAmount: Num = new Num(1, 0);
+  holdingDisplay: HoldingDisplay = HoldingDisplayFactory.start(this)
+    .withAmountPrefix('You have')
+    .withAmountSuffix('Yellow Power')
+    .withEffectPrefix('They multiply red generators by')
+    .build()
+
+  override action(): Num | undefined {
+    // @ts-ignore
+    let buffer: Num = new Num(1, 0);
+    if (this.amount.greq(new Num(1, 0))) {
+      // @ts-ignore
+      buffer = amount.pow(GlobalMultipliersService.get('yellowPowerPower'), false).add(new Num(1, 0), false);
+    }
+    GlobalMultipliersService.correct('redParticleGenerators', buffer);
+    return buffer;
+  }
+
+  getStyle(): Styles {
+    return Styles.YELLOW;
+  }
+
+  override effectString(effect: Num): string {
+    return super.effectString(effect) + 'x';
+  }
 }
