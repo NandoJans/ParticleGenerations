f52b830563632cf71b3ab15fe50f54785c2d8484
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 18:13:40 +0200

    applyNewKnowledge Added yellow phase holdings

--- src/app/classes/records/holdings/holding-record.ts
+++ src/app/classes/records/holdings/holding-record.ts
@@ -1,7 +1,21 @@
 import {RedParticleHolding} from "../../features/holdings/red-particle-holding";
 import {RedAcceleratorHolding} from "../../features/holdings/red-accelerator-holding";
+import {YellowParticleHolding} from "../../features/holdings/yellow-particle-holding";
+import {YellowHolding} from "../../features/holdings/yellow-holding";
+import {YellowPowerHolding} from "../../features/holdings/yellow-power-holding";
+import {YellowFusionHolding} from "../../features/holdings/yellow-fusion-holding";
 
 export class HoldingRecord {
+
+  // Red Phase
   static redParticles     = new RedParticleHolding()
   static redAccelerators  = new RedAcceleratorHolding()
+
+  // Yellow Phase
+  static yellowParticles  = new YellowParticleHolding()
+  static yellows          = new YellowHolding()
+  static yellowPower      = new YellowPowerHolding()
+  static yellowFusion     = new YellowFusionHolding()
+
+  // Green Phase
 }
