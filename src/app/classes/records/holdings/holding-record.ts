6b6c16986e88d84134b0a50f5ae20e186634adfe
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 18:23:01 +0200

    applyNewKnowledge Add green phase holdings

--- src/app/classes/records/holdings/holding-record.ts
+++ src/app/classes/records/holdings/holding-record.ts
@@ -4,6 +4,13 @@ import {YellowParticleHolding} from "../../features/holdings/yellow-particle-hol
 import {YellowHolding} from "../../features/holdings/yellow-holding";
 import {YellowPowerHolding} from "../../features/holdings/yellow-power-holding";
 import {YellowFusionHolding} from "../../features/holdings/yellow-fusion-holding";
+import {GreenParticleHolding} from "../../features/holdings/green-particle-holding";
+import {GreenHolding} from "../../features/holdings/green-holding";
+import {GreenEnergyHolding} from "../../features/holdings/green-energy-holding";
+import {GreenSoulsHolding} from "../../features/holdings/green-souls-holding";
+import {DarkEnergyHolding} from "../../features/holdings/dark-energy-holding";
+import {DarkPowerHolding} from "../../features/holdings/dark-power-holding";
+import {NuclearDecayHolding} from "../../features/holdings/nuclear-decay-holding";
 
 export class HoldingRecord {
 
@@ -18,4 +25,11 @@ export class HoldingRecord {
   static yellowFusion     = new YellowFusionHolding()
 
   // Green Phase
+  static greenParticles   = new GreenParticleHolding()
+  static greens           = new GreenHolding()
+  static greenEnergy      = new GreenEnergyHolding()
+  static greenSouls       = new GreenSoulsHolding()
+  static darkEnergy       = new DarkEnergyHolding()
+  static darkPower        = new DarkPowerHolding()
+  static nuclearDecay     = new NuclearDecayHolding()
 }
