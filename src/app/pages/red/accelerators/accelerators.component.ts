12f12c770f8283ea931fe03022c78947cdc2e927
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 19:07:11 +0200

    applyNewKnowledge Use the new holding functionality

--- src/app/pages/red/accelerators/accelerators.component.ts
+++ src/app/pages/red/accelerators/accelerators.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Generator, Upgrade} from "../../../globals";
 import {UpgradeService} from "../../../services/interactables/upgrade.service";
 import {GeneratorService} from "../../../services/interactables/generator.service";
+import {HoldingRecord} from "../../../classes/records/holdings/holding-record";
 
 @Component({
   selector: 'app-accelerators',
@@ -15,7 +16,9 @@ export class AcceleratorsComponent implements OnInit {
     'Red accelerators is a new type of currency used for accelerating red generators. The multiplier to red generators is equal to some factor of the red accelerators.' +
     ' They are able to be boosted by three different companions which all give a multiplier to the "single" red accelerator generator.'
   ]
-  constructor() { }
+  constructor(
+    public holdingRecord: HoldingRecord
+  ) { }
 
   ngOnInit(): void {
     this.generators = GeneratorService.getGenerators('red-accelerators');
