0b103596b927aaffa4b9f19acf710986bc87c3cd
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 22:12:17 +0200

    applyNewKnowledge Create holdingdisplay line

---
+++ src/app/interfaces/holding-display-line.spec.ts
@@ -0,0 +1,7 @@
+import { HoldingDisplayLine } from './holding-display-line';
+
+describe('HoldingDisplayLine', () => {
+  it('should create an instance', () => {
+    expect(new HoldingDisplayLine()).toBeTruthy();
+  });
+});
