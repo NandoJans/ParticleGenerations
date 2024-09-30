a4a7cc93cff80dc967ed38d29dabb39c6262c6c9
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 19:01:37 +0200

    applyNewKnowledge Use the new holding functionality

--- src/app/pages/yellow/yellow-generators/yellow-generators.component.ts
+++ src/app/pages/yellow/yellow-generators/yellow-generators.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GeneratorService} from "../../../services/interactables/generator.service";
 import {Generator} from "../../../globals";
+import {HoldingRecord} from "../../../classes/records/holdings/holding-record";
 
 @Component({
   selector: 'app-yellow-generators',
@@ -10,7 +11,9 @@ import {Generator} from "../../../globals";
 export class YellowGeneratorsComponent implements OnInit {
   generators: Generator[] = [];
 
-  constructor() { }
+  constructor(
+    public holdingRecord: HoldingRecord
+  ) { }
 
   ngOnInit(): void {
     this.generators = GeneratorService.getGenerators('yellow-particles');
