019bcd710be359f9ad56381db5046cb480135cbf
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-09-30 17:50:54 +0200

    applyNewKnowledge Create classes for yellow phase holdings

---
+++ src/app/classes/features/holdings/yellow-fusion-holding.spec.ts
@@ -0,0 +1,7 @@
+import { YellowFusionHolding } from './yellow-fusion-holding';
+
+describe('YellowFusionHolding', () => {
+  it('should create an instance', () => {
+    expect(new YellowFusionHolding()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/features/holdings/yellow-holding.spec.ts
@@ -0,0 +1,7 @@
+import { YellowHolding } from './yellow-holding';
+
+describe('YellowHolding', () => {
+  it('should create an instance', () => {
+    expect(new YellowHolding()).toBeTruthy();
+  });
+});
