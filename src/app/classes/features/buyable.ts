319692b567da20f2a2c3eb1844f6bfef61c5b7b8
NandoJans <n.jans@st.hanze.nl>
2024-10-01 15:01:22 +0200

    applyNewKnowledge Implement buyable properties

--- src/app/classes/features/buyable.ts
+++ src/app/classes/features/buyable.ts
@@ -1,7 +1,12 @@
 import {Num} from "../../num";
 
-export interface Buyable {
-  bought: Num;
-  currency: string;
+export abstract class Buyable {
+  abstract baseCost: Num
+  abstract cost: Num
+  abstract increase: Num
+  abstract scalingStart: Num
+  scaling?: Num
+  abstract bought: Num
+  abstract currency: string
   // TODO: add the methods from buyable service
 }
