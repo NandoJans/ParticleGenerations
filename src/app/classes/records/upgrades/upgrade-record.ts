c34550a3bf0d6c107e0cc0845a05efb4c73be26f
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-10-02 13:49:43 +0200

    applyNewKnowledge Added upgrades to record

--- src/app/classes/records/upgrades/upgrade-record.ts
+++ src/app/classes/records/upgrades/upgrade-record.ts
@@ -1,13 +1,25 @@
 import {RedGeneratorExtensionUpgrade} from "../../features/upgrades/red-generator-extension-upgrade";
 import {RedGeneratorBoosterUpgrade} from "../../features/upgrades/red-generator-booster-upgrade";
+import {FirstRedAcceleratorMultiplierUpgrade} from "../../features/upgrades/first-red-accelerator-multiplier-upgrade";
+import {SecondRedAcceleratorMultiplierUpgrade} from "../../features/upgrades/second-red-accelerator-multiplier-upgrade";
+import {ThirdRedAcceleratorMultiplierUpgrade} from "../../features/upgrades/third-red-accelerator-multiplier-upgrade";
 
 export class UpgradeRecord {
   // Red Upgrades
   static redGeneratorExtension: RedGeneratorExtensionUpgrade = new RedGeneratorExtensionUpgrade();
   static redGeneratorBooster: RedGeneratorBoosterUpgrade = new RedGeneratorBoosterUpgrade();
 
+  static firstRedAcceleratorMultiplier: FirstRedAcceleratorMultiplierUpgrade = new FirstRedAcceleratorMultiplierUpgrade();
+  static secondRedAcceleratorMultiplier: SecondRedAcceleratorMultiplierUpgrade = new SecondRedAcceleratorMultiplierUpgrade();
+  static thirdRedAcceleratorMultiplier: ThirdRedAcceleratorMultiplierUpgrade = new ThirdRedAcceleratorMultiplierUpgrade();
+
+  static
 
   getRedGeneratorExtension(): RedGeneratorExtensionUpgrade {
     return UpgradeRecord.redGeneratorExtension;
+  }
+
+  getRedGeneratorBooster(): RedGeneratorBoosterUpgrade {
+    return UpgradeRecord.redGeneratorBooster;
   }
 }
