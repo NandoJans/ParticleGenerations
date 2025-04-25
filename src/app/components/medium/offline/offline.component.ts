818cc94f1251fbea2509665c6cd9c25c5ee19745
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-04-25 23:33:20 +0200

    9 Offline window to view the generation

---
+++ src/app/components/medium/offline/offline.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {OfflineService} from "../../../services/offline.service";
+
+@Component({
+  selector: 'app-offline',
+  templateUrl: './offline.component.html',
+  styleUrls: ['./offline.component.css']
+})
+export class OfflineComponent implements OnInit {
+
+  constructor(
+    private offlineService: OfflineService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  getOfflineProgress(): number {
+    const progress = this.offlineService.getTickProgress() / this.offlineService.getTotalTicks() * 100;
+    return 100 - Math.min(progress, 100);
+  }
+
+  getTickProgress(): number {
+    return this.offlineService.getTicksDone();
+  }
+
+  getTotalTicks(): number {
+    return this.offlineService.getTotalTicks();
+  }
+
+  close() {
+    this.offlineService.close();
+  }
+
+  isDone() {
+    return this.offlineService.isDone();
+  }
+}
