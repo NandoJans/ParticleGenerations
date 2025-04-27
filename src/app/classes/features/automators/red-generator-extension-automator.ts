891f2b44e4e5bbb9df69edec6f89ef84f93dbef3
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-04-27 23:48:37 +0200

    Losen the automator requirement

--- src/app/classes/features/automators/red-generator-extension-automator.ts
+++ src/app/classes/features/automators/red-generator-extension-automator.ts
@@ -9,8 +9,8 @@ import {StatsService} from "../../../services/stats.service";
 
 export class RedGeneratorExtensionAutomator extends Automator {
   displayName: string = 'Extension Automator';
-  goal: Num = new Num(1, 3);
-  goalString: string = 'Reset red generators a total of 1.000 times';
+  goal: Num = new Num(1, 2);
+  goalString: string = 'Reset red generators a total of 100 times';
   name: string = 'red-generator-extension-automator';
   style: Styles = Styles.RED_AUTOMATOR;
   override unlocked: boolean = true;
