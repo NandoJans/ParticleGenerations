4a7bf4d51be4dd11a9e0e4cea6f38a43ad027ea7
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-05-08 12:51:39 +0200

    Update sirius-star-challenge.ts

--- src/app/classes/features/challenges/sirius-star-challenge.ts
+++ src/app/classes/features/challenges/sirius-star-challenge.ts
@@ -53,7 +53,7 @@ export class SiriusStarChallenge extends YellowStarChallenge {
     this.challengeGenerators['siriusGenerator'].amount = new Num(1, 0);
     this.challengeGenerators['siriusGenerator'].bought = new Num(1, 0);
 
-    this.siriusGeneratorMultiplier = HoldingRecord.redParticles.amount.pow(new Num(1, -3)).mul(this.challengeUpgrades['siriusGeneratorMultiplierUpgrade'].effect);
+    this.siriusGeneratorMultiplier = HoldingRecord.redParticles.amount.pow(new Num(1, -3)).mul(this.challengeUpgrades['siriusGeneratorMultiplierUpgrade'].effect ?? new Num(1, 0));
     this.challengeGenerators['siriusGenerator'].multiplier = this.siriusGeneratorMultiplier.copy();
 
     const sunParticleEffect: Num = this.challengeHoldings['siriusParticle'].amount.pow(new Num(3, 0)).floor();
