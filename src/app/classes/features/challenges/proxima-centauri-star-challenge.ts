a8a2dec454e08f421acf0d6527494e715e50432b
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-05-12 20:31:53 +0200

    Update proxima-centauri-star-challenge.ts

--- src/app/classes/features/challenges/proxima-centauri-star-challenge.ts
+++ src/app/classes/features/challenges/proxima-centauri-star-challenge.ts
@@ -85,7 +85,7 @@ export class ProximaCentauriStarChallenge extends YellowStarChallenge {
         HoldingRecord.redAccelerators,
         this.style,
         GeneratorRecord.thirdRedGenerator,
-        this.getDifficultyIncrease(new Num(1.9, 0)),
+        this.getDifficultyIncrease(new Num(1.8, 0)),
       ),
       unlockFourthRedGenerator: new YellowStarChallengeUnlockUpgrade(
         'unlockFourthRedGenerator',
@@ -95,7 +95,7 @@ export class ProximaCentauriStarChallenge extends YellowStarChallenge {
         HoldingRecord.redParticles,
         this.style,
         GeneratorRecord.fourthRedGenerator,
-        this.getDifficultyIncrease(new Num(0.8, 0)),
+        this.getDifficultyIncrease(new Num(0.7, 0)),
       ),
       unlockFifthRedGenerator: new YellowStarChallengeUnlockUpgrade(
         'unlockFifthRedGenerator',
@@ -105,7 +105,7 @@ export class ProximaCentauriStarChallenge extends YellowStarChallenge {
         HoldingRecord.redAccelerators,
         this.style,
         GeneratorRecord.fifthRedGenerator,
-        this.getDifficultyIncrease(new Num(0.75, 0)),
+        this.getDifficultyIncrease(new Num(0.65, 0)),
       ),
     }
   }
