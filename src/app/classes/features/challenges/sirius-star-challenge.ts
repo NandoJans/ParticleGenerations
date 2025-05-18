75bce736eedd623e98ec69dbb50025d576ffa473
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-05-18 22:33:24 +0200

    Update sirius-star-challenge.ts

--- src/app/classes/features/challenges/sirius-star-challenge.ts
+++ src/app/classes/features/challenges/sirius-star-challenge.ts
@@ -37,7 +37,7 @@ export class SiriusStarChallenge extends YellowStarChallenge {
     new Num(1, 0),
     new Num(1.05, 0),
     new Num(0.87, 0),
-    new Num(0.7, 0),
+    new Num(0.69, 0),
   ];
 
   getRewardDescription(): string {
