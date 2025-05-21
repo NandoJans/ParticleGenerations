858b9326344f04077291e94a2a0422390c374c3b
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-05-21 07:59:46 +0200

    Update booster-acceleration-upgrade.ts

--- src/app/classes/features/upgrades/booster-acceleration-upgrade.ts
+++ src/app/classes/features/upgrades/booster-acceleration-upgrade.ts
@@ -13,8 +13,8 @@ import { Enhancement } from "../enhancements/enhancement";
 import {EnhancementRecord} from "../../records/enhancement-record";
 
 export class BoosterAccelerationUpgrade extends Upgrade {
-  baseCost: Num = new Num(1, 4);
-  cost: Num = new Num(1, 4);
+  baseCost: Num = new Num(1, 3);
+  cost: Num = new Num(1, 3);
   increase: Num = new Num(1, 2);
   override scaling = new Num(1, 3);
   override scalingStart: Num = new Num(1, 15);
