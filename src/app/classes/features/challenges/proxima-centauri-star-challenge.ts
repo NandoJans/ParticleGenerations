a15237f87dabf5f8d0f399b8584a99c76f1de3a7
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-05-29 19:17:34 +0200

    Update proxima-centauri-star-challenge.ts

--- src/app/classes/features/challenges/proxima-centauri-star-challenge.ts
+++ src/app/classes/features/challenges/proxima-centauri-star-challenge.ts
@@ -24,7 +24,7 @@ export class ProximaCentauriStarChallenge extends YellowStarChallenge {
   override difficultyIncrease: Num[] = [
     new Num(1, 0),
     new Num(4, 0),
-    new Num(3, 0),
+    new Num(4, 0),
     new Num(2, 0),
   ];
 
