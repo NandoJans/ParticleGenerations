89204acdf2edbbf7c162ca0761f8f0421648d6ab
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-06-01 13:20:36 +0200

    Update increase-sirius-goal-upgrade.ts

--- src/app/classes/features/upgrades/increase-sirius-goal-upgrade.ts
+++ src/app/classes/features/upgrades/increase-sirius-goal-upgrade.ts
@@ -21,8 +21,8 @@ export class IncreaseSiriusGoalUpgrade extends IncreaseChallengeGoalUpgrade {
   baseCost: Num = new Num(1, 30);
   cost: Num = new Num(1, 30);
 override scaling: Num = new Num(1, 15)
-  increase: Num = new Num(1, 8);
-  startIncrease: Num = new Num(1, 8);
+  increase: Num = new Num(1, 12);
+  startIncrease: Num = new Num(1, 12);
   override limit: Num = new Num(5, 0);
   currency: Holding = HoldingRecord.yellowParticles;
   requirement: Requirement[] = [
