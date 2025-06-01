438dc45616965afb9c9099c5a203a355a0afa398
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-06-01 13:19:34 +0200

    Update increase-proxima-centauri-goal-upgrade.ts

--- src/app/classes/features/upgrades/increase-proxima-centauri-goal-upgrade.ts
+++ src/app/classes/features/upgrades/increase-proxima-centauri-goal-upgrade.ts
@@ -20,7 +20,7 @@ export class IncreaseProximaCentauriGoalUpgrade extends IncreaseChallengeGoalUpg
   subNav: string = 'yellowFusion';
   baseCost: Num = new Num(1, 10);
   cost: Num = new Num(1, 10);
-  override scaling: Num = new Num(1, 5)
+  override scaling: Num = new Num(1, 9)
   increase: Num = new Num(1, 15);
   startIncrease: Num = new Num(1, 5);
   currency: Holding = HoldingRecord.yellowParticles;
