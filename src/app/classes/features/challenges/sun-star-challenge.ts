6beca749a6ee64f68c54b20c65da8aa804d60d37
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-11-22 20:03:53 +0100

    Update sun-star-challenge.ts

--- src/app/classes/features/challenges/sun-star-challenge.ts
+++ src/app/classes/features/challenges/sun-star-challenge.ts
@@ -65,11 +65,11 @@ export class SunStarChallenge extends YellowStarChallenge {
   rewardSoftCap: Num = new Num(1, 300_000)
 
   reward(): Num {
-    const effect = HoldingRecord.yellowPower.effect?.pow(this.buffer) ?? new Num(1, 0)
+    let effect = HoldingRecord.yellowPower.effect?.pow(this.buffer) ?? new Num(1, 0)
     if (effect.gt(this.rewardSoftCap)) {
       // Calculate the cap of the reward.
       const cappedEffect = effect.div(this.rewardSoftCap);
-        return this.rewardSoftCap.mul(cappedEffect.sqrt());
+        effect = this.rewardSoftCap.mul(cappedEffect.sqrt());
     }
     MultiplierRecord.redAcceleratorGenerators.correct(effect);
     return effect;
