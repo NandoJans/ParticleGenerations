600ace65bb2cca10f23651da777a62b4841aa96a
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-02-23 12:22:32 +0100

    Update sirius-star-challenge.ts

--- src/app/classes/features/challenges/sirius-star-challenge.ts
+++ src/app/classes/features/challenges/sirius-star-challenge.ts
@@ -393,7 +393,7 @@ export class SiriusStarChallenge extends YellowStarChallenge {
       ),
     }
 this.requirement = [
-    new Requirement(HoldingRecord.redParticles, new Num(1, 11000), this)
+    new Requirement(HoldingRecord.redParticles, new Num(1, 20000), this)
   ];
   }
 }
