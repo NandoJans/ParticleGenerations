1d603d4dfd489f8f51dda3b34bac5bc12e663caf
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-03-08 22:37:18 +0100

    Update sirius-star-challenge.ts

--- src/app/classes/features/challenges/sirius-star-challenge.ts
+++ src/app/classes/features/challenges/sirius-star-challenge.ts
@@ -178,7 +178,7 @@ export class SiriusStarChallenge extends YellowStarChallenge {
         'yellowStars',
         'siriusUpgrade',
         this.challengeGenerators['siriusGenerator'].globalMultiplier,
-        this.getDifficultyIncrease(new Num(0.7, 0)),
+        this.getDifficultyIncrease(new Num(0.6, 0)),
       ),
       unlockFirstRedGeneratorMultiplierUpgrade: new YellowStarChallengeUnlockUpgrade(
         'unlockFirstRedGeneratorMultiplierUpgrade',
