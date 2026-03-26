e0a80beee9bc92e05b76ba59d09d213f73949616
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-03-26 17:13:39 +0100

    Increase buffer values for Fused Acceleration upgrade
    Updated buffer and baseBuffer values for Fused Acceleration upgrade.

--- src/app/classes/features/upgrades/fused-acceleration-galaxy-tree-upgrade.ts
+++ src/app/classes/features/upgrades/fused-acceleration-galaxy-tree-upgrade.ts
@@ -41,8 +41,8 @@ export class FusedAccelerationGalaxyTreeUpgrade extends GalaxyTreeUpgrade {
   style: Styles = Styles.STAR_YELLOW;
   displayName: string = "Fused Acceleration";
 
-  override buffer = new Num(2, 0);
-  override baseBuffer = new Num(2, 0);
+  override buffer = new Num(5, 0);
+  override baseBuffer = new Num(5, 0);
 
   cost: Num = new Num(1.5, 1);
   baseCost: Num = new Num(1.5, 1);
