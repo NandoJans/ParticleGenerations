9f75d9c6266bbcc770ba6700d9d204174badad49
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-03-26 15:50:43 +0100

    Reduce cost and baseCost values for upgrade

--- src/app/classes/features/upgrades/fused-acceleration-galaxy-tree-upgrade.ts
+++ src/app/classes/features/upgrades/fused-acceleration-galaxy-tree-upgrade.ts
@@ -44,6 +44,6 @@ export class FusedAccelerationGalaxyTreeUpgrade extends GalaxyTreeUpgrade {
   override buffer = new Num(0.5, 0);
   override baseBuffer = new Num(0.5, 0);
 
-  cost: Num = new Num(2, 1);
-  baseCost: Num = new Num(2, 1);
+  cost: Num = new Num(1.5, 1);
+  baseCost: Num = new Num(1.5, 1);
 }
