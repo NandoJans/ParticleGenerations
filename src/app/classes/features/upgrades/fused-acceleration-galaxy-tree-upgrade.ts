7434eeb5b6d0b9f179cf4a7a8816fc888fff3d42
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-03-27 08:19:07 +0100

    Update fused-acceleration-galaxy-tree-upgrade.ts

--- src/app/classes/features/upgrades/fused-acceleration-galaxy-tree-upgrade.ts
+++ src/app/classes/features/upgrades/fused-acceleration-galaxy-tree-upgrade.ts
@@ -53,8 +53,8 @@ export class FusedAccelerationGalaxyTreeUpgrade extends GalaxyTreeUpgrade {
   style: Styles = Styles.STAR_YELLOW;
   displayName: string = "Fused Acceleration";
 
-  override buffer = new Num(2, 1);
-  override baseBuffer = new Num(2, 1);
+  override buffer = new Num(5, 1);
+  override baseBuffer = new Num(5, 1);
 
   cost: Num = new Num(1.5, 1);
   baseCost: Num = new Num(1.5, 1);
