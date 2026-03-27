cfbabe667659636b69e8a33abca1a298ad1e002a
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-03-27 13:41:40 +0100

    Update power-acceleration-galaxy-tree-upgrade.ts

--- src/app/classes/features/upgrades/power-acceleration-galaxy-tree-upgrade.ts
+++ src/app/classes/features/upgrades/power-acceleration-galaxy-tree-upgrade.ts
@@ -38,8 +38,8 @@ export class PowerAccelerationGalaxyTreeUpgrade extends GalaxyTreeUpgrade {
   }
 
   // Softcap settings - tune these values to adjust where slowdown starts and how strong it is.
-  slowdownStart: Num = new Num(1, 250);
-  slowdownPower: Num = new Num(5, -1);
+  slowdownStart: Num = new Num(1, 350);
+  slowdownPower: Num = new Num(7.5, -1);
 
   private applySlowdown(effect: Num): Num {
     if (effect.greq(this.slowdownStart)) {
