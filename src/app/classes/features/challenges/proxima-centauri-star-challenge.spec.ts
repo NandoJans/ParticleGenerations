2f3540a33229334a080879a490470420947193f9
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-04-02 11:17:02 +0200

    Fix challenge reset to rebuild star requirements and limits

--- src/app/classes/features/challenges/proxima-centauri-star-challenge.spec.ts
+++ src/app/classes/features/challenges/proxima-centauri-star-challenge.spec.ts
@@ -1,7 +1,24 @@
 import { ProximaCentauriStarChallenge } from './proxima-centauri-star-challenge';
+import { Num } from '../../../num';
 
 describe('ProximaCentauriStarChallenge', () => {
   it('should create an instance', () => {
     expect(new ProximaCentauriStarChallenge()).toBeTruthy();
+  });
+
+  it('should re-initialize challenge upgrades and max completions on reset', () => {
+    const challenge = new ProximaCentauriStarChallenge();
+    challenge.completed = new Num(3, 0);
+    challenge.init();
+
+    const upgradeCostBeforeReset = challenge.challengeUpgrades['unlockSecondRedGenerator'].baseCost.copy();
+
+    challenge.maxCompletions = new Num(5, 0);
+    challenge.reset();
+
+    expect(challenge.maxCompletions).toBeUndefined();
+
+    const upgradeCostAfterReset = challenge.challengeUpgrades['unlockSecondRedGenerator'].baseCost;
+    expect(upgradeCostAfterReset.lt(upgradeCostBeforeReset)).toBeTrue();
   });
 });
