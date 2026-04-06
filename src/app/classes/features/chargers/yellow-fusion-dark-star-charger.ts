1b47661e6e2af05a9c560267818cb716bd498d38
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-04-06 09:34:28 +0200

    Update yellow-fusion-dark-star-charger.ts

--- src/app/classes/features/chargers/yellow-fusion-dark-star-charger.ts
+++ src/app/classes/features/chargers/yellow-fusion-dark-star-charger.ts
@@ -79,7 +79,7 @@ export class YellowFusionDarkStarCharger extends DarkStarCharger {
     );
 
     // Hydrogen generation boost: (0.1 × charge + 1), additionally shared-tier amplified.
-    const baseHydrogenBoost = Num.ONE.add(totalCharge.mul(new Num(0.3, 0)));
+    const baseHydrogenBoost = Num.ONE.add(totalCharge.mul(new Num(0.25, 0).mul(this.tier.sub(Num.ONE))));
     this.hydrogenGenerationBoost = this.applySharedTierBoost(baseHydrogenBoost);
     MultiplierRecord.hydrogenGenerators.correct(this.hydrogenGenerationBoost);
 
