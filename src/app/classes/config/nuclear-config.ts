195379dd25afc56df4cb69a6ac5a4f8be5dc1095
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-06-11 00:23:08 +0200

    Changes to power

--- src/app/classes/config/nuclear-config.ts
+++ src/app/classes/config/nuclear-config.ts
@@ -22,7 +22,7 @@ export class NuclearConfig {
   static readonly potentialDarkStarPower = new Num(5, -1);
   static readonly potentialTierWeight = new Num(1, -1);
   static readonly fissionPerPotentialPerSecond = new Num(1, 0);
-  static readonly fissionBoosterLogarithmFactor = new Num(5, -3);
+  static readonly fissionBoosterPower = new Num(5, -3);
 
   static readonly upgrades: NuclearUpgradeConfig[] = [
     {
