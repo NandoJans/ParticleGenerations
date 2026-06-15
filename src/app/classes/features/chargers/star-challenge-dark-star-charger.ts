ffaa8e6c87654196de35a24907420169a945a4e9
NandoJans <90688887+NandoJans@users.noreply.github.com>
2026-06-15 18:00:58 +0200

    Update star-challenge-dark-star-charger.ts

--- src/app/classes/features/chargers/star-challenge-dark-star-charger.ts
+++ src/app/classes/features/chargers/star-challenge-dark-star-charger.ts
@@ -30,8 +30,9 @@ export class StarChallengeDarkStarCharger extends DarkStarCharger {
   requirement: Requirement[] = [];
   name: string = 'star-challenge-dark-star-charger';
   override tierNerf: Num[] = [
-    new Num(0.9, 0),
-    new Num(0.5, 0),
+    new Num(0.9, 0), // TIER II
+    new Num(0.85, 0), // TIER III
+    new Num(0.8, 0), // TIER IV
   ];
 
   /**
